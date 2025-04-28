a776bae9ae9fc71f0a24ac176e3aed6c7950b07e
Alexis <alexismanuelperezperez742002@gmail.com>
2025-04-27 20:12:30 -0400

    agregado footer del menu lateral

--- src/medicare/components/Menu.jsx
+++ src/medicare/components/Menu.jsx
@@ -2,7 +2,7 @@ import { MenuItem } from "./MenuItem"
 
 export const Menu = () => {
     return (
-        <aside className="w-64 bg-[var(--primary-color)] text-[var(--light-color)]">
+        <aside className="w-64 bg-[var(--primary-color)] text-[var(--light-color)] flex flex-col">
             <header className="w-full h-20 border-b border-gray-600">
                 <span className="flex items-center h-full gap-2 pl-5">
                     <img src="/medicare/logo.svg" alt="MediCare logo"
@@ -22,6 +22,26 @@ export const Menu = () => {
                 <MenuItem text="Recetas" type="medications" path="/medications" />
                 <MenuItem text="Configuracion" type="settings" path="/settings" />
             </nav>
+
+            <footer className="flex-1 flex items-end">
+                <div className="border-t border-gray-600 h-26.5 w-full flex pl-5">
+                    <div className="flex pt-5 justify-center gap-2.5 cursor-pointer">
+                        <span>
+                            <img src="/medicare/placeholder-user.png" alt="User icon"
+                                className="w-10.5 h-10.5 rounded-full border border-gray-600"
+                            />
+                        </span>
+                        <span className="flex flex-col gap-0.5">
+                            <strong>
+                                Dr. Juan Pérez
+                            </strong>
+                            <p className="text-xs text-gray-300">
+                                Cardiólogo
+                            </p>
+                        </span>
+                    </div>
+                </div>
+            </footer>
         </aside>
     )
 }
